7f8ecedf1bf6201fc4b8049656caf9afb3e349fc
tanduong <tanduong@live.com>
2020-02-13 07:25:00 -0600

    PLAT 1223

--- ContactsBlueprint/navigator.js
+++ ContactsBlueprint/navigator.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import Contacts from "./screens/contacts";
 import Home from "./screens";
 
-export const ContactsBlueprintNavigator = createStackNavigator(
+export default ContactsBlueprintNavigator = createStackNavigator(
   {
     Home: { screen: Home },
     Contacts: { screen: Contacts },
